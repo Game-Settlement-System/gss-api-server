ff3415d70ba8099aaed56ef4372ccae48d5b8c27
goyounha11 <goyounha11@naver.com>
2022-07-01 14:49:15 +0900

    initialize gss-api-server

---
+++ src/main/java/com/gss/GssApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.gss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GssApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GssApiServerApplication.class, args);
+    }
+
+}
